8b1f96c58ac42100509ecf7753528fe64b034e4a
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-01-31 14:32:58 +0700

    feat: add request body validation for create and update endpoints

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import databaseClient from "./services/database.mjs";
 import { ObjectId } from "mongodb";
+import { requestSchema } from "./models/utils.js";
 
 const HOSTNAME = process.env.SERVER_IP || "127.0.0.1";
 const PORT = process.env.SERVER_PORT || 3000;
@@ -39,6 +40,13 @@ webServer.get("/activities/:activityId", async (req, res) => {
 
 // Create a new activity
 webServer.post("/activities", async (req, res) => {
+  // validiate request body
+  const { error } = requestSchema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // insert activity into database
   const userId = new ObjectId(req.body.userId);
   const activity = { ...req.body, userId };
   const returnedActivity = { activityId: "", ...activity };
@@ -57,6 +65,13 @@ webServer.post("/activities", async (req, res) => {
 
 // Update an activity
 webServer.put("/activities/:activityId", async (req, res) => {
+  // validiate request body
+  const { error } = requestSchema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // update activity in database
   const activityId = req.params.activityId;
   const activity = req.body;
   activity.userId = new ObjectId(activity.userId);
