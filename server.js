f93f39221318e0c5071378d34ea44ceb7e911643
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-02-06 10:20:04 +0700

    chore: clean up comments in server.js file

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ import morgan from "morgan";
 import databaseClient from "./services/database.mjs";
 import * as activityControllers from "./controllers/activityControllers.js";
 
-// TODO: update status codes appropriately
-// const HOSTNAME = process.env.SERVER_HOSTNAME;
 const PORT = process.env.SERVER_PORT || 3000;
 
 dotenv.config();
