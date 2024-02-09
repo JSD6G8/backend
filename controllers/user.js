0506c19a950393950715d9b76b35786364396bde
totsapol_n <totsapol.piluk@gmail.com>
2024-02-09 11:31:17 +0700

    fix bug  respon(send error)

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ import databaseClient from "../services/database.mjs";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { requestUser, requestUserLogin } from "./userrequest.js";
-import auth from "../middleware/auth.js"
+import auth from "../middleware/auth.js";
 
 // register
 export const userRegister = async (req, res) => {
@@ -42,10 +42,10 @@ export const userRegister = async (req, res) => {
       .collection("user")
       .findOne({ emailAddress });
 
-      const jwtSecretKey = process.env.TOKEN_KEY;
-      const token = jwt.sign({ id: emailAddress }, jwtSecretKey, {
-        expiresIn: "2h",
-      });
+    const jwtSecretKey = process.env.TOKEN_KEY;
+    const token = jwt.sign({ id: emailAddress }, jwtSecretKey, {
+      expiresIn: "2h",
+    });
 
     //save token
     user.token;
@@ -55,8 +55,6 @@ export const userRegister = async (req, res) => {
     console.log(error);
   }
 };
-
-
 
 // Login
 export const userLogin = async (req, res) => {
@@ -76,23 +74,23 @@ export const userLogin = async (req, res) => {
       .findOne({ emailAddress });
 
     if (user && (await bcrypt.compareSync(password, user.password))) {
-        const jwtSecretKey = process.env.TOKEN_KEY;
-        const token = jwt.sign({ id: emailAddress }, jwtSecretKey, {
-          expiresIn: "2h",
-        });
-      
-      user.token;
-      res.status(200).json(token);
-    }
+      const jwtSecretKey = process.env.TOKEN_KEY;
+      const token = jwt.sign({ id: emailAddress }, jwtSecretKey, {
+        expiresIn: "2h",
+      });
 
-    res.status(400).send("Invalid email or password");
+      user.token;
+      res.status(201).json({ user, token });
+    } else {
+      res.status(400).send("Invalid email or password");
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
 export const tokenLogin = (req, res) => {
-    res.status(200).send("Welcome");
-  };
-  
+  res.status(200).send("Welcome");
+};
+
 export const protectedTokenLogin = [auth, tokenLogin];
