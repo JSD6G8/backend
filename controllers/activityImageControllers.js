75c8bf384af3258ceecb06a63682755403e2cc27
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-02-11 15:02:47 +0700

    feat: add create activity image functionality

---
+++ controllers/activityImageControllers.js
@@ -0,0 +1,32 @@
+import databaseClient from "../services/database.mjs";
+import { ObjectId } from "mongodb";
+import { v2 as cloudinary } from "cloudinary";
+
+export const createActivityImage = async (req, res) => {
+  // validate if activityId is a valid ObjectId
+  if (!ObjectId.isValid(req.params.activityId)) {
+    return res.status(400).send("Invalid activityId");
+  }
+
+  // insert image into database
+  const activityId = new ObjectId(req.params.activityId);
+  const image = {
+    url: req.cloudinary.secure_url,
+    publicId: req.cloudinary.public_id,
+  }
+
+  try {
+    const result = await databaseClient
+      .db()
+      .collection("activities")
+      .updateOne(
+        { _id: activityId },
+        { $push: { images: image } }
+      );
+    res.status(201).send({
+        result,
+        });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import databaseClient from "./services/database.mjs";
+import { v2 as cloudinary } from "cloudinary";
 import * as activityControllers from "./controllers/activityControllers.js";
+import * as activityImageControllers from "./controllers/activityImageControllers.js";
 
 const MODE = process.env.NODE_ENV || "production";
 const PORT = process.env.SERVER_PORT || 3000;
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+async function uploadToCloudinary(req, res, next) {
+  const fileBufferBase64 = Buffer.from(req.file.buffer).toString("base64");
+  const base64File = `data:${req.file.mimetype};base64,${fileBufferBase64}`;
+  req.cloudinary = await cloudinary.uploader.upload(base64File, {
+    resource_type: "auto",
+  });
+
+  next();
+}
 
 const webServer = express();
 webServer.use(cors());
@@ -28,20 +50,26 @@ webServer.post("/activities", activityControllers.createActivity);
 webServer.put("/activities/:activityId", activityControllers.updateActivity);
 webServer.delete("/activities/:activityId", activityControllers.deleteActivity);
 
+webServer.post(
+  "/activities/:activityId/images",
+  upload.single("image"),
+  uploadToCloudinary,
+  activityImageControllers.createActivityImage
+);
+
 // initialize web server
-if (MODE === 'development') {
+if (MODE === "development") {
   const currentServer = webServer.listen(PORT, "localhost", () => {
-    console.log(`Database connected: ${databaseClient.db().databaseName}`)
+    console.log(`Database connected: ${databaseClient.db().databaseName}`);
   });
 } else {
   const currentServer = webServer.listen(PORT, () => {
-    console.log(`Database connected: ${databaseClient.db().databaseName}`)
+    console.log(`Database connected: ${databaseClient.db().databaseName}`);
   });
 }
 
-
 // clean up on exit
-function cleanup () {
+function cleanup() {
   currentServer.close(() => {
     console.log("Server closed.");
     try {
@@ -50,7 +78,7 @@ function cleanup () {
     } catch (err) {
       console.error(err);
     }
-  })
+  });
 }
 
 process.on("SIGTERM", cleanup);
